9f8a239a2aad9008c7e2f6e43b3b997bbb8e5ec2
Rodrigo Souza <ro6rigo@gmail.com>
2025-05-08 11:17:25 -0300

    Initial commit

---
+++ src/main/java/me/dio/dev_week_2025/DevWeek2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.dev_week_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeek2025Application.class, args);
+	}
+
+}
